febddea927f43edc762c91a9d9098f6a00f4ca36
mroxanam <mroxanam20@gmail.com>
2023-04-27 16:00:57 -0300

    multiplcacion

---
+++ operacion.py
@@ -0,0 +1,9 @@
+# Pedimos al usuario que ingrese los números a multiplicar
+numero1 = int(input("Ingrese el primer número: "))
+numero2 = int(input("Ingrese el segundo número: "))
+
+# Realizamos la multiplicación y la almacenamos en la variable resultado
+resultado = numero1 * numero2
+
+# Imprimimos el resultado por consola
+print("El resultado de la multiplicación es:", resultado)
