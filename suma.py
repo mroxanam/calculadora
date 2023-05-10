25483561b1a05bcd37ef4fd8e1661cf8c3e5530e
nataliaaberghina <nataliaaberghina17@gmail.com>
2023-05-10 19:48:36 -0300

    suma

---
+++ suma.py
@@ -0,0 +1,9 @@
+# Pedimos al usuario que ingrese los números suma
+numero1 = int(input("Ingrese el primer número: "))
+numero2 = int(input("Ingrese el segundo número: "))
+
+# Realizamos la suma y la almacenamos en la variable resultado
+resultado = numero1 + numero2
+
+# Imprimimos el resultado por consola
+print("El resultado de la suma es:", resultado)
